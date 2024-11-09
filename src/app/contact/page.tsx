84f65a8e5cfc5074116b261cfbb8e12014f3539c
KHIZRA-IQBAL <iqbalkhizra026@gmail.com>
2024-11-10 00:20:02 +0500

    Your commit message here

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { FaPhone } from "react-icons/fa6";
-import { FaArrowRight } from "react-icons/fa6";
 export default function Contact(){
     return(
         <div className="h-[37.5rem] w-[100] flex">
@@ -11,9 +9,9 @@ export default function Contact(){
             <div className="w-[50%] border bg-slate-800 text-center pt-40">
                 <h1 className="text-6xl font-bold p3-4">Lets talk</h1>
                 <p className="pb-5  text-[20px]">share your excitment with us</p>
-                 <div className="flex justify-center"><p className="text-[1.0625rem] font-semibold">info@ABZaaT@gmail.com </p><span className="mt-2 text-[17px]"><FaArrowRight/></span></div>
+                 <div className="flex justify-center"><p className="text-[1.0625rem] font-semibold">info@ABZaaT@gmail.com </p><span className="mt-2 text-[17px]"></span></div>
                 <div className="flex justify-center">
-                   <span className="mt-2 text-[18px]"><FaPhone/></span> <h1 className="text-[20px]">Contact Us: </h1>
+                   <span className="mt-2 text-[18px]"></span> <h1 className="text-[20px]">Contact Us: </h1>
                     <p className="mt-1"> 03-0013729648</p>
                 </div>
             </div>
